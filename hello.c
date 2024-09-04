b1b373f4e22168b10a4e6c1b126f67806f8cacff
Mihir-Maruvada <mihir.maruvada@gmail.com>
2024-09-03 21:42:30 -0500

    Added C code to file

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Mihir Lochan Maruvada
+ * Date: 2024/09/23
+ * CSCE 155H
+ */
+
+#include <stdio.h>
+
+int main() {
+
+printf("My name is Mihir Lochan Maruvada!\n");
+printf("I'm majoring in Computer Science\n");
+
+  return 0;
+}
