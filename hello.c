a0e7fe3d7c85bfab092d499e5e6f1a8dfdbaedeb
Yohannes <yhailu006@gmail.com>
2024-09-03 23:06:47 -0500

    Added my name and major

--- hello.c
+++ hello.c
@@ -10,6 +10,8 @@ int main() {
 
 printf("My name is Mihir Lochan Maruvada!\n");
 printf("I'm majoring in Computer Science\n");
+printf("My name is Yohannes Hailu!\n");
+printf("I'm majoring in Computer Science and Math\n");
 
   return 0;
 }
